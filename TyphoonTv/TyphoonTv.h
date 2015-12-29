5e99a9e1bcc6030d1573065dc6fb54a9e296887b
a169674 <a169674@externos.bankia.com>
2015-12-29 14:37:22 +0100

    Support AppleTV

---
+++ TyphoonTv/TyphoonTv.h
@@ -0,0 +1,19 @@
+//
+//  TyphoonTv.h
+//  TyphoonTv
+//
+//  Created by Fidel on 21/12/15.
+//  Copyright © 2015 typhoonframework.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TyphoonTv.
+FOUNDATION_EXPORT double TyphoonTvVersionNumber;
+
+//! Project version string for TyphoonTv.
+FOUNDATION_EXPORT const unsigned char TyphoonTvVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TyphoonTv/PublicHeader.h>
+
+
